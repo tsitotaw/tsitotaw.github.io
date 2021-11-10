cca4dd21cae24533963ead437faef14906d1d550
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2021-11-10 08:33:37 -0600

    minor tweaks

--- jquery/check.js
+++ jquery/check.js
@@ -1,10 +1,82 @@
-let indexer = 0;
-$('span').parentsUntil('body').each(function(index) {
-    if(indexer < 2)
-    {
-        $(this).css({
-            "backgroundColor": 'green'
-        });
-        indexer++;
-    }
-});
+
+$("<div><h1>JQuery Core</h1></div>").appendTo(document.body);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  $("div").has("ul").css("color", "blue");
+
+//  var ulList = function(){ 
+
+//     $("div").each(function(){ 
+    
+//     if($.contains(this,document.getElementById("check"))){ 
+    
+//         $(this).css("color","blue") 
+    
+//     } 
+    
+    
+//     }); 
+    
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let pContent = $('p').html();
+// $('p').append(" YES!");
+// $("p").append("<p> Yes!!</p>");
+// $("<span>YES!</span>").insertAfter("p");
+// $("p").append("<span> YES!</span>");
+// $('div > h1').css("background-color", "red");
+// $('span').parentsUntil('body').each(function(index) {
+//     if(indexer < 2)
+//     {
+//         $(this).css({
+//             "backgroundColor": 'green'
+//         });
+//         indexer++;
+//     }
+// });
